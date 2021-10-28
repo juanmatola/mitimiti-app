ce56148d6ad1ef94075849f3d9a39acef48c0618
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-28 20:49:20 -0300

    Recibo data para creacion de usuario encontrolador

--- src/main/java/com/example/mitimiti/controllers/FrontController.java
+++ src/main/java/com/example/mitimiti/controllers/FrontController.java
@@ -3,7 +3,9 @@ package com.example.mitimiti.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.mitimiti.config.ViewNames;
 import com.example.mitimiti.util.ErrorHandler;
@@ -25,6 +27,12 @@ public class FrontController implements ErrorHandler {
 	@GetMapping("/singup")
 	public String singup () {
 		return ViewNames.SING_UP;
+	}
+	
+	@PostMapping("/sing-up")
+	public String singupPost(@RequestParam("username") String username, @RequestParam("password") String password) {
+		
+		return "redirect:/";
 	}
 
 	@Override
