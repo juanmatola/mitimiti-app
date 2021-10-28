6b90d0ea4aa562897fca53d891a9bc32cb91a311
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-10-28 12:07:49 -0300

    Cargo nuevas vistas a ViewNames config

--- src/main/java/com/example/mitimiti/config/ViewNames.java
+++ src/main/java/com/example/mitimiti/config/ViewNames.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public final class ViewNames {
 	public static final String INDEX = "index";
+	public static final String LOGIN = "login";
+	public static final String SING_UP = "sing-up";
 }
