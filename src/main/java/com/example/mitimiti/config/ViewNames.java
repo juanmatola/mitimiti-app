3d8338371f518f62d12b84053b4e2d84fe8c886c
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-11-01 20:57:47 -0300

    Cambio manejo de vista de login y sing-up con una sola vista y modales desplegables

--- src/main/java/com/example/mitimiti/config/ViewNames.java
+++ src/main/java/com/example/mitimiti/config/ViewNames.java
@@ -5,6 +5,4 @@ import org.springframework.stereotype.Component;
 @Component
 public final class ViewNames {
 	public static final String INDEX = "index";
-	public static final String LOGIN = "login";
-	public static final String SING_UP = "sing-up";
 }
