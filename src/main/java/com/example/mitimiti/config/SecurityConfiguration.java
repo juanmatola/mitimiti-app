de8b737f23ef72310134f0f9abcaa0bd2d97b1b5
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-11-01 21:20:28 -0300

    Limpio imports no utilizadoas

--- src/main/java/com/example/mitimiti/config/SecurityConfiguration.java
+++ src/main/java/com/example/mitimiti/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.example.mitimiti.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
