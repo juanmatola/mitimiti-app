063f97ca267b0031cf61bc30cfaaaf0ccca17825
juanmatola <tolajuanmanuel@yahoo.com.ar>
2021-11-11 12:55:23 -0300

    Correccion en configuracion de seguridad para permitir el registro

--- src/main/java/com/example/mitimiti/config/SecurityConfiguration.java
+++ src/main/java/com/example/mitimiti/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/css/**", "/js/**", "/img/**", "/").permitAll()
+				.antMatchers("/css/**", "/js/**", "/img/**", "/", "/sing-up").permitAll()
 				.antMatchers("/**").authenticated()
 				.and().formLogin().loginPage("/?action=login").loginProcessingUrl("/auth")
 				.usernameParameter("username").passwordParameter("password")
